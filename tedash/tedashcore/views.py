c4982e8d44dabe2cc75c508cd6779b59e448f740
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2023-10-01 20:52:18 +0200

    Add pre-commit

--- tedash/tedashcore/views.py
+++ tedash/tedashcore/views.py
@@ -18,12 +18,13 @@ def report_junit_xml(request):
     if not form.is_valid():
         return HttpResponseBadRequest()
 
-    project, created = models.Project.objects.get_or_create(name=request.POST["project_name"])
+    project, created = models.Project.objects.get_or_create(
+        name=request.POST["project_name"]
+    )
 
     importer = JunitImporter(request.FILES["file"])
     metadata = importer.metadata
-    now = datetime.datetime.now()
-    report = models.Report.objects.create(
+    models.Report.objects.create(
         project=project,
         name=metadata["name"],
         tests=metadata["tests"],
@@ -31,28 +32,27 @@ def report_junit_xml(request):
         errors=metadata.get("errors"),
         skipped=metadata.get("skipped"),
         assertions=metadata.get("assertions"),
-        time=datetime.timedelta(seconds=metadata.get("duration" , 0)),
+        time=datetime.timedelta(seconds=metadata.get("duration", 0)),
         created=datetime.datetime.fromisoformat(metadata.get("timestamp")),
         report=importer.data,
     )
 
     return render(request, "import_report.html", {"msg": "Imported"})
 
+
 @require_http_methods(["GET"])
 def projects(request):
     projects = models.Project.objects.all()
     return render(request, "projects.html", {"projects": projects})
+
 
 @require_http_methods(["GET"])
 def project(request, name):
     project = models.Project.objects.prefetch_related().get(name=name)
     return render(request, "project-detail.html", {"project": project})
 
+
 @require_http_methods(["GET"])
 def report(request, project_name, id):
     report = models.Report.objects.prefetch_related().get(pk=id)
-    import json
-    print(json.dumps(report.report, indent=2))
     return render(request, "report-detail.html", {"report": report})
-
-
